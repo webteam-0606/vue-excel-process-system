ec1cc587f2879513cab2ed2f213c1ae050f7821a
chen-June06 <278990214@qq.com>
2022-07-14 10:04:40 +0800

    去除页面右边滑页，加了两个CT数据

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -84,7 +84,7 @@
                   将文件拖到此处,或
                   <em>点击上传</em>
                 </div>
-                <div class="el-upload__tip" slot="tip">1次只能上传1个xls文件,最多上传50个</div>
+                <div class="el-upload__tip" slot="tip">1次可上传多个xls文件,最多上传50个</div>
               </el-upload>
             </div>
           </div>
