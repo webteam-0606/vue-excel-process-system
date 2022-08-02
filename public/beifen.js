bad7ae04960bf19bb775ddaa25c368ccac8b1dfb
chen-June06 <278990214@qq.com>
2022-08-02 15:11:05 +0800

    服务器备份

---
+++ public/beifen.js
@@ -0,0 +1,13 @@
+// Obsoleted:
+// Yy241937
+// User: 310211810
+// Password: EEInterns@202205
+
+// Production:
+// cnhszhphc1ws303
+// User: 310211810
+// Password: EEInterns@202205
+
+// Web: 
+// Production: http://cnhszhphc1ws303
+// Test: http://cnhszhphc1ws303:9001
