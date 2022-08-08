34af205b56dbd1a2284b0278d1394d81940166fc
chen-June06 <278990214@qq.com>
2022-08-08 10:30:42 +0800

    合并导入excel的代码

---
+++ import-excel/README.md
@@ -0,0 +1,47 @@
+## excel 自动导入数据库步骤
+> 利用 Postgres 数据库，MySQL数据库，Navicat 16 工具，VScode工具。
+
+1、进入文件夹，打开对应项目。
+> 启动 `vscode`，文件 -> 打开文件夹 -> 选择 `vue-excel-process-system-master` 文件夹，打开项目。然后点击 `import-excel` -> `excelTodatabase.py`。
+
++ 注意：如需上传的excel表名与数据库已有表重名，请到 步骤4，删除旧的重复的表。再进行一下操作。
+
+2、修改 `excel` 路径，以及对应数据库名称。
+> 点击 `excelTodatabase.py`， 修改以下内容：
+```java
+// 其中path为需要导入数据库的excel的路径。
+// database 为导入excel的数据库名称。
+def createtable():
+    path='C:/Users/320200255/project/import-excel/test.xlsx'
+    ...
+    conn = psycopg2.connect(database='postgres-uploadsearch', user='postgres', password='postgres', host='localhost')
+    ...
+```
+3、运行项目。
+> 修改好，点击保存。右键 `Run code`，运行。
+
+4、打开 `postgreSQL` 数据库，刷新表。
+> 打开电脑应用程序 `postgreSQL 11` 中的 `pgAdmin 4` 应用，账户密码都为 `postgres`。依次点击 `postgres-uploadsearch` -> `postgres-uploadsearch` -> `public` -> 表。右键 `表`, 进行刷新，即可看到 `excel` 中的每个 `sheet` 表。
+
+5、将 `postgreSQL` 中的表复制到 `MySql`。
+> 将 `postgreSQL` 中的表复制到 `MySql`。本地 `excel` 导入 `postgreSQL` 数据库完成。
+---------------------------
+## vue 实现多表查找搜索
+1、进入文件夹，打开对应项目。
+> 启动 `vscode`，文件 -> 打开文件夹 -> 选择 `vue-excel-process-system-master` 文件夹。
+
+2、修改 server API。index.js。router.js。
+> ...
+
+3、启动服务器。
+> 终端输入 cd server。终端输入 node app.js。若显示 服务器启动成功 则ok。
+
+4、启动项目。
+> 终端界面，点击 powershell 右边的 + 后，终端再输入 npm run dev 启动项目。网页输入生成的地址，即可。
+
+5、打包项目。
+> 终端输入 ctrl+c 停止项目，终端输入 npm run build 打包项目。此时项目目录会自动更新dist文件夹。将dist文件夹复制到内网，替换原先的 `dist` 文件夹。
+
+6、内网访问项目。
+> 依次点击 `public` 文件夹 -> `dist` 文件夹 -> `index.html`。即可打开网页，进行搜索。
+
---
+++ import-excel/excelTodatabase.py
@@ -0,0 +1,108 @@
+import psycopg2
+# import postgres
+import xlrd
+from datetime import datetime
+from xlrd import xldate_as_tuple
+#根据有多少个sheets去创建多少个表，path为excel表格的路径
+def createtable():
+    # 读取excel
+    path='C:/Users/320200255/project/import-excel/test.xlsx'
+    data = xlrd.open_workbook(path)
+    # 根据sheet索引获取sheet的内容
+    # print("excel全部的sheet为:", data.sheet_names())
+    # print("all sheet in excel:", data.sheet_names())
+    sheet_names = data.sheet_names()
+    table_one = data.sheet_by_index(0)
+    # print("all cols name of a single sheet:", table_one.row_values(0))
+    conn = psycopg2.connect(database='postgres-uploadsearch', user='postgres', password='postgres', host='localhost')
+    cur = conn.cursor()
+    for i in range(0, len(sheet_names)):
+        #当前sheet的名字
+        print('sheet_names[i]---:',sheet_names[i])
+        table_name = sheet_names[i]
+        print('table_name:---',table_name)
+        # 当前的sheet
+        now_table = data.sheet_by_index(i)
+        # 获得当前sheet的列数就是 属性数
+        cols_num = now_table.ncols
+        # 获得当前表格的行数，就是有多少的数据量
+        rows_num = now_table.nrows
+        print('rows_num:',rows_num)
+        # 获得当前的属性的数组，其实就是第一例的值
+        attrs = now_table.row_values(0)
+        #判断表格是否存在
+        cur.execute("SELECT to_regclass('%s') is not null" % table_name)
+        flag = cur.fetchone()[0]
+        print('flag',flag)
+        if flag :
+            # print('存在了,直接将表的内容插入')
+            print('existed,insert the content of table directly')
+            # 将当前的sheet插入到数据库
+            for k in range(1, rows_num):
+                row_vlaue = now_table.row_values(k)
+                # print('row_vlaue:',row_vlaue)
+                # print('join(attrs):',','.join(attrs))
+                # 处理要插入的数据，把非字符串的数据转换成字符串类型，同事将字符串变成 sql语句需要的类型
+                for a in range(0, len(row_vlaue)):
+                    ctype = now_table.cell(k, a).ctype
+                    # print('ctype', ctype)
+                    #ctype： 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+                    if ctype ==2 and  row_vlaue[a] % 1 ==0 :
+                        tmp = int(row_vlaue[a])
+                        row_vlaue[a] = str(tmp)
+                    if ctype == 3 :
+                        d = datetime(*xldate_as_tuple(row_vlaue[a],0))
+                        row_vlaue[a] = d.strftime('%Y-%m-%d')
+                    c = row_vlaue[a]
+                    row_vlaue[a] = "'" + str(c) + "'"
+                # print('join(row_vlaue):',','.join(row_vlaue))
+                # print('table_name:',table_name)
+                sql = "INSERT INTO %s(%s) VALUES(%s)" % (table_name, ','.join(attrs), ','.join(row_vlaue))
+                # sql = "INSERT INTO %s(%s) VALUES(%s)"
+                print(sql)
+                # print("before excute---------")
+                cur.execute(sql)
+                # print("after excute----------")
+                conn.commit()
+        else:
+            print('table_name:',table_name)
+            cur.execute("CREATE TABLE " + table_name + "();")
+            conn.commit()
+            # 为sheet进行建表，
+            # cur.execute("ALTER TABLE %s ADD COLUMN  id SERIAL primary key  ;" % table_name)
+            # conn.commit()
+
+            # cur.execute("CREATE SEQUENCE users_id_seq  START WITH 1  INCREMENT BY 1  NO MINVALUE  NO MAXVALUE  CACHE 1;" )
+            # conn.commit()
+
+            # cur.execute("alter table  %s alter column id set default nextval('users_id_seq'); " % table_name)
+            # conn.commit()
+            for j in range(0, cols_num):
+                cur.execute("ALTER TABLE %s ADD COLUMN %s VARCHAR;" % (table_name, attrs[j]))
+                conn.commit()
+            # 将当前的sheet插入到数据库
+            for k in range(1, rows_num):
+                row_vlaue = now_table.row_values(k)
+                # print('row_vlaue:',row_vlaue)
+                print('join(attrs):',','.join(attrs))
+                # 处理要插入的数据，把非字符串的数据转换成字符串类型，同事将字符串变成 sql语句需要的类型
+                for a in range(0, len(row_vlaue)):
+                    ctype = now_table.cell(k, a).ctype
+                    # print('ctype', ctype)
+                    # ctype： 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+                    # if ctype == 2 and row_vlaue[a] % 1 == 0:
+                    #     tmp = int(row_vlaue[a])
+                    #     row_vlaue[a] = str(tmp)
+                    # if ctype == 3:
+                    #     d = datetime(*xldate_as_tuple(row_vlaue[a], 0))
+                    #     row_vlaue[a] = d.strftime('%Y-%m-%d')
+                    c = row_vlaue[a]
+                    row_vlaue[a] = "'" + str(c) + "'"
+                # print(','.join(row_vlaue))
+                sql = "INSERT INTO %s(%s) VALUES(%s)" % (table_name, ','.join(attrs), ','.join(row_vlaue))
+                # print(sql)
+                cur.execute(sql)
+                conn.commit()
+    conn.close()
+
+createtable()
